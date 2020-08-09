77785b42d73db77df4fb09235eb92de40482a476
FarDean <sfh1212@gmail.com>
2020-08-09 12:00:08 +0430

    added accordion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const tabItems= Array.from(document.querySelectorAll('.tab-item'));
 const tabContentItems = Array.from(document.querySelectorAll('.tab-content-item'));
-
+const accHeadContent = Array.from(document.querySelectorAll('.acc-head-content'));
+const accHead = Array.from(document.querySelectorAll('.acc-head'));
 function selectItem(e){
     for(let i = 0;i<tabItems.length;i++){
         tabItems[i].classList.remove("tab-border");
@@ -18,3 +19,16 @@ tabItems.forEach((item,index)=> item.addEventListener('click',(e)=>{
     item.classList.add('tab-border');
     tabContentItems[index].classList.add('active');
 }))
+
+accHead.forEach((head,index)=> {
+    head.addEventListener('click',(e)=>{
+        if(head.nextElementSibling.classList.contains('active')){
+            head.nextElementSibling.classList.remove('active')
+        }else{
+            for(let i= 0;i<accHead.length;i++){
+                accHeadContent[i].classList.remove('active');
+            }
+            accHeadContent[index].classList.toggle('active')
+        }
+    })
+})
